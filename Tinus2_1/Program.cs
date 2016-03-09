e2760ca5bc5997d02bf5cf2e9a7cdb92ccd69fc7
GovjadkoIlja <govjadkoilja@yandex.ru>
2016-03-09 19:28:12 +0300

    .

---
+++ Tinus2_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tinus2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gary, lary;
+            string str = Console.ReadLine();
+            var res = str.Split(' ').Select(item => Convert.ToInt32(item)).ToArray();
+            gary = res[0];
+            lary = res[1];
+            gary--;
+            lary--;
+            Console.Write(lary + " " + gary);
+            Console.ReadKey();
+        }
+    }
+}
